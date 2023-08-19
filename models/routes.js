212c0daf7f83233b77e9126176d479a319cdb980
RicardoFreire1409 <107887249+RicardoFreire1409@users.noreply.github.com>
2023-08-19 12:44:16 -0500

    Update routes.js

--- models/routes.js
+++ models/routes.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/router_db', {
+mongoose.connect('mongodb+srv://jorftama:180522@cluster0.yercuev.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
 },
 (err) => {
